9ee17a544fd079530554a3fc2dd8cba2cfd4109f
Dang Thi Thuy  Tien <tiendang@TienDangs-MacBook.local>
2022-03-12 18:13:55 +0700

    skelation for story two photos

---
+++ js/main.js
@@ -0,0 +1,90 @@
+(function () {
+    "use strict";
+
+    /**
+     * Easy selector helper function
+     */
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    /**
+     * Easy event listener function
+     */
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+
+    /**
+     * Easy on scroll event listener
+     */
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    /**
+     * Toggle .header-scrolled class to #header when page is scrolled
+     */
+    let selectHeader = select('#header')
+    if (selectHeader) {
+        const headerScrolled = () => {
+            if (window.scrollY > 100) {
+                selectHeader.classList.add('header-scrolled')
+            } else {
+                selectHeader.classList.remove('header-scrolled')
+            }
+        }
+        window.addEventListener('load', headerScrolled)
+        onscroll(document, headerScrolled)
+    }
+
+
+    /**
+     * Back to top button
+     */
+    let backtotop = select('.back-to-top')
+    if (backtotop) {
+        const toggleBacktotop = () => {
+            if (window.scrollY > 100) {
+                backtotop.classList.add('active')
+            } else {
+                backtotop.classList.remove('active')
+            }
+        }
+        window.addEventListener('load', toggleBacktotop)
+        onscroll(document, toggleBacktotop)
+    }
+
+})()
+
+$(document).ready(function () {
+    'use strict';
+
+    $(window).scroll(function () {
+        var scrollDistance = $(window).scrollTop();
+        var hlUnderLine = $('.hl-under-line');
+        var offSet = $(window).height() * 75 / 100;
+
+        /* Under line of News Detail Story */
+        if (hlUnderLine.position().top <= (scrollDistance + offSet)) {
+            hlUnderLine.addClass('active-scroll-under-line');
+        }
+        var staticUnderLine = $('.statistic-under-line');
+        if (staticUnderLine.position().top <= (scrollDistance + offSet)) {
+            staticUnderLine.addClass('active-scroll-under-line');
+        }
+
+    }).scroll();
+})
